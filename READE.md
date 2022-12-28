94f09107728be9132bdeaeb55365ac9fefe0d692
Arthur Guardieiro <arthur.aaguardieiro@icloud.com>
2022-12-28 09:17:49 -0300

    Atualizando README e app.js

--- READE.md
+++ READE.md
@@ -1,7 +1,6 @@
 # Sistema de cadastro.
 > Status do projeto: em desenvolvimento
 
-Para rodar o projeto digite:
 ```
 node app.js
 ```
--- app.js
+++ app.js
@@ -1,2 +1,3 @@
 console.log("Hello world");
 cosnole.log("aplicação está se conectando ao banco de dados");
+console.log("git clone utilizado");
